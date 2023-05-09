72986974536b19bafc30cc335e24ffb4ba1ab1e6
kozakevich13 <hanibalsmitti@gmail.com>
2023-05-09 13:53:56 +0300

    add some style to PokemonDetails

--- src/components/PokemonCards.js
+++ src/components/PokemonCards.js
@@ -1,45 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Card from './Card';
-import PokemonDetails from './PokemonDetails'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./Card";
+import PokemonDetails from "./PokemonDetails";
 
 const PokemonCards = () => {
   const [pokemonList, setPokemonList] = useState([]);
-  const [nextPageUrl, setNextPageUrl] = useState('');
+  const [nextPageUrl, setNextPageUrl] = useState("");
   const [selectedPokemon, setSelectedPokemon] = useState(null);
   const [test, setTest] = useState(null);
 
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon/4/')
-      .then(response => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/4/")
+      .then((response) => {
         setTest(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  
-  console.log(test)
+  console.log(test);
 
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=12')
-      .then(response => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=12")
+      .then((response) => {
         setPokemonList(response.data.results);
         setNextPageUrl(response.data.next);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }, []);
 
   const handleLoadMore = () => {
-    axios.get(nextPageUrl)
-      .then(response => {
+    axios
+      .get(nextPageUrl)
+      .then((response) => {
         setPokemonList([...pokemonList, ...response.data.results]);
         setNextPageUrl(response.data.next);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   };
@@ -55,21 +57,21 @@ const PokemonCards = () => {
   return (
     <div>
       {selectedPokemon ? (
-        <PokemonDetails pokemon={selectedPokemon} onClose={handleDetailsClose} />
+        <PokemonDetails
+          pokemon={selectedPokemon}
+          onClose={handleDetailsClose}
+        />
       ) : (
-        <>
-          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-            {pokemonList.map(pokemon => (
-              <div key={pokemon.name} style={{ width: '33.33%', padding: '10px' }}>
-                <Card pokemon={pokemon} onClick={() => handleCardClick(pokemon)} />
-              </div>
-            ))}
-          </div>
-          {nextPageUrl && (
-            <button onClick={handleLoadMore}>Load More</button>
-          )}
-        </>
+        <> </>
       )}
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {pokemonList.map((pokemon) => (
+          <div key={pokemon.name} style={{ width: "33.33%", padding: "10px" }}>
+            <Card pokemon={pokemon} onClick={() => handleCardClick(pokemon)} />
+          </div>
+        ))}
+      </div>
+      {nextPageUrl && <button onClick={handleLoadMore}>Load More</button>}
     </div>
   );
 };
--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,10 +1,23 @@
 const PokemonDetails = ({ pokemon, onClose }) => (
-    <div>
+    <div style={{
+        position: 'fixed',
+        top: '50%',
+        right: '5%',
+        transform: 'translateY(-50%)',
+        width: '30%',
+        backgroundColor: 'white',
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+        padding: '1rem',
+        zIndex: 9999 // set a higher z-index value
+      }}>
       <h2>{pokemon.name}</h2>
       <button onClick={onClose}>Close</button>
+      {/* <img src={pokemon.sprites.front_default} alt={pokemon.name} /> */}
       <p>Height: {pokemon.height}</p>
       <p>Weight: {pokemon.weight}</p>
+      {/* <p>Type: {pokemon.types[0].type.name}</p> */}
     </div>
   );
 
   export default PokemonDetails
+  
