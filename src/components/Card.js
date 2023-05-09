4bd21f8e6b902df153142338f77be7ade2ab7dab
kozakevich13 <hanibalsmitti@gmail.com>
2023-05-09 20:06:31 +0300

    add style to type of pokemon

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,17 +4,21 @@ import axios from "axios";
 const Card = ({ pokemon, onClick }) => {
   const { name, url } = pokemon;
   const [pokemonDetails, setPokemonDetails] = useState(null);
+  const [pokemonType, setPokemonType] = useState("");
 
   useEffect(() => {
     axios
       .get(url)
       .then((response) => {
         setPokemonDetails(response.data);
+        setPokemonType(response.data.types[0].type.name);
       })
       .catch((error) => {
         console.log(error);
       });
   }, [url]);
+
+  console.log(pokemonType);
 
   return (
     <div className="card" onClick={onClick}>
@@ -24,10 +28,38 @@ const Card = ({ pokemon, onClick }) => {
       <div className="card-content">
         <p className="card-name">{name}</p>
         {pokemonDetails && (
-          <p className="card-type">
-            Type:{" "}
-            {pokemonDetails.types.map((type) => type.type.name).join(", ")}
-          </p>
+          <div className="type-container">
+            {pokemonDetails.types.map((type) => (
+              <div
+                className={`pokemon-type card-${
+                  type.type.name === "grass"
+                    ? "grass"
+                    : type.type.name === "poison"
+                    ? "poison"
+                    : type.type.name === "fire"
+                    ? "fire"
+                    : type.type.name === "water"
+                    ? "water"
+                    : type.type.name === "flying"
+                    ? "water"
+                    : type.type.name === "bug"
+                    ? "bug"
+                    : type.type.name === "normal"
+                    ? "normal"
+                    : type.type.name === "electric"
+                    ? "electric"
+                    : type.type.name === "ground"
+                    ? "ground"
+                    : type.type.name === "fairy"
+                    ? "fairy"
+                    : ""
+                }`}
+                key={type.id}
+              >
+                {type.type.name}
+              </div>
+            ))}
+          </div>
         )}
       </div>
     </div>
